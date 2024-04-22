cfceda4eef70418b02466458de4cd72d2e53d6ec
Matheus da Silva <matheus.silva@nl.com.br>
2024-04-22 13:43:56 -0300

    feat: novas aulas

--- node/temporizador.js
+++ node/temporizador.js
@@ -1,5 +1,19 @@
 const schedule = require("node-schedule");
 
 const t1 = schedule.scheduleJob("*/5*****", function () {
-  console.log("exec..");
+  console.log("exec..", new Date().getSeconds());
+});
+
+setTimeout(function () {
+  t1.cancel();
+  console.log("Cancelando tarefa 1");
+}, 10000);
+
+const regra = new schedule.RecurrenceRule();
+regra.dayOfWeek = [new schedule.Range(1, 5)];
+regra.hour = 12;
+regra.second = 10;
+
+const t2 = new schedule.scheduleJob(regra, function () {
+  console.log("Executando t2", new Date().getSeconds());
 });
