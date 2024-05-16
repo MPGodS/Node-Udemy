1bbc9bce19515ab483220fdfc53b8d808fcd7bd3
Matheus da Silva <matheus.silva@nl.com.br>
2024-05-16 09:02:10 -0300

    feat: concluido gulp

--- gulp_inicial/gulp/spa/gulpTasks/app.js
+++ gulp_inicial/gulp/spa/gulpTasks/app.js
@@ -6,21 +6,41 @@ const sass = require("gulp-sass")(require("sass"));
 const htmlmin = require("gulp-htmlmin");
 const babel = require("gulp-babel");
 
-function appHTML(cb) {
-  return cb();
+function appHTML() {
+  return gulp
+    .src("src/**/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
 }
 
-function appCSS(cb) {
-  return cb();
+function appCSS() {
+  return gulp
+    .src("src/assets/sass/index.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(uglifycss({ uglyComments: true }))
+    .pipe(concat("app.min.css"))
+    .pipe(gulp.dest("build/assets/css"));
 }
 
-function appJS(cb) {
-  return cb();
+function appJS() {
+  return gulp
+    .src("src/assets/js/**/*.js")
+    .pipe(babel({ presets: ["env"], comments: false }))
+    .pipe(uglify())
+    .pipe(concat("app.min.js"))
+    .pipe(gulp.dest("build/assets/js"));
 }
 
-function appIMG(cb) {
-  return cb();
+function appIMG() {
+  return gulp
+    .src("src/assets/imgs/**/*.*")
+    .pipe(gulp.dest("build/assets/imgs"));
 }
+
+gulp.task("appHTML", appHTML);
+gulp.task("appCSS", appCSS);
+gulp.task("appJS", appJS);
+gulp.task("appIMG", appIMG);
 
 module.exports = {
   appHTML,
