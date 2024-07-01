5d28f0965ced941c7fcbe4fccaac65e1aaad000f
UCSMatheusDaSilva <MPGSilva1@ucs.br>
2024-07-01 14:25:16 -0300

    feat: Calculadora vue

---
+++ Projetos/Projeto_Calculadora-Vue/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>Calculadora</h1>
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from "./main/Calculator";
+export default {
+  components: {
+    Calculator,
+  },
+};
+</script>
+
+<style>
+* {
+  font-family: monospace;
+}
+body {
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  text-align: center;
+
+  color: white;
+  background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+}
+</style>
